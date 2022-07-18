1744fdf4cf9992c743b94fa76b3ad61c1c66f088
Brandon Block <brandon.block@pm.me>
2022-07-18 15:52:57 -0500

    list recipes endpoint

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"time"
 
@@ -21,6 +23,10 @@ type Recipe struct {
 // Store recipes in memory for initial routes
 var recipes []Recipe
 
+func ListRecipesHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, recipes)
+}
+
 func NewRecipeHandler(c *gin.Context) {
 	var recipe Recipe
 
@@ -36,13 +42,17 @@ func NewRecipeHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipe)
 }
 
+// while recipes are held in memory, we'll load them from a JSON file at startup
 func init() {
 	recipes = make([]Recipe, 0)
+	file, _ := ioutil.ReadFile("recipes.json")
+	_ = json.Unmarshal([]byte(file), &recipes)
 }
 
 func main() {
 	router := gin.Default()
 
+	router.GET("/recipes", ListRecipesHandler)
 	router.POST("/recipes", NewRecipeHandler)
 
 	router.Run()
