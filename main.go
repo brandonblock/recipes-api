27119e91236ce4b4abb1b05d922c161523598831
Brandon Block <brandon.block@pm.me>
2022-07-19 18:24:31 -0500

    routes and DB (#11)
    * boilerplate
    
    * recipe data model (#1)
    
    * initial recipe endpoint (#2)
    
    * list recipes endpoint (#3)
    
    * update recipe endpoint (#4)
    
    * delete recipe endpoint (#5)
    
    * search recipe by tag (#6)
    
    * Feature/openapi (#7)
    
    * add open api comments
    
    * add Makefile for swagger gen & run
    
    * gitignore
    
    * Feature/mongo integration (#9)
    
    * add mongo image
    
    * use mongo instead of in-memory recipe storage
    
    * Fix/code refactor (#10)
    
    * organize via handlers, models
    
    * clean up globals
    
    * clean up comments

--- main.go
+++ main.go
@@ -16,187 +16,69 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
-	"net/http"
-	"strings"
-	"time"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/rs/xid"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+
+	handlers "recipes-api/handlers"
+	models "recipes-api/models"
 )
 
-//Recipe is the data model for the recipes our API handles
-type Recipe struct {
-	ID           string    `json:"id"`
-	Name         string    `json:"name"`
-	Tags         []string  `json:"tags"`
-	Ingredients  []string  `json:"ingredients"`
-	Instructions []string  `json:"instructions"`
-	PublishedAt  time.Time `json:"publishedAt"`
-}
+// var collection *mongo.Collection
+var recipesHandler *handlers.RecipesHandler
 
-// Store recipes in memory for initial routes
-var recipes []Recipe
-
-// swagger:operation POST /recipes recipes createRecipe
-// Creates a new recipe
-// ---
-// produces:
-// - application/json
-// responses:
-// 200':
-// description: Successful operation
-func NewRecipeHandler(c *gin.Context) {
-	var recipe Recipe
-
-	if err := c.ShouldBindJSON(&recipe); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	recipe.ID = xid.New().String()
-	recipe.PublishedAt = time.Now()
-	recipes = append(recipes, recipe)
-
-	c.JSON(http.StatusOK, recipe)
-}
-
-// swagger:operation GET /recipes recipes listRecipes
-// Returns list of recipes
-// ---
-// produces:
-// - application/json
-// responses:
-// 200':
-//  description: Successful operation
-// '400':
-//  description: Invalid input
-func ListRecipesHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, recipes)
-}
-
-// swagger:operation GET /recipes/search recipes searchRecipes
-// Returns list of recipes
-// ---
-// produces:
-// - application/json
-// responses:
-// '200':
-//  description: Successful operation
-func SearchRecipesHandler(c *gin.Context) {
-	tag := c.Query("tag")
-	result := make([]Recipe, 0)
-
-	for _, r := range recipes {
-		found := false
-		for _, t := range r.Tags {
-			if strings.EqualFold(t, tag) {
-				found = true
-			}
-		}
-		if found {
-			result = append(result, r)
-		}
-	}
-	c.JSON(http.StatusOK, result)
-}
-
-// swagger:operation PUT /recipes/{id} recipes updateRecipe
-// Update and existing recipe
-// ---
-// parameters:
-// - name: id
-//   in: path
-//   description: ID of the recipe
-//   required: true
-//   type: string
-// produces:
-// - application/json
-// responses:
-//  '200':
-//    description: Successful operation
-//  '400':
-//    description: Invalid input
-//  '404':
-//    description: Invalid recipe ID
-func UpdateRecipesHandler(c *gin.Context) {
-	id := c.Param("id")
-	var recipe Recipe
-	if err := c.ShouldBindJSON(&recipe); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	//nasty iteration because we're storing in a huge array
-	index := -1
-	for i, r := range recipes {
-		if r.ID == id {
-			index = i
-		}
-	}
-	if index == -1 {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Recipe Not Found"})
-		return
-	}
-	recipes[index] = recipe
-
-	c.JSON(http.StatusOK, recipe)
-}
-
-// swagger:operation DELETE /recipes/{id} recipes deleteRecipes
-// Deletes target recipe by id
-// ---
-// parameters:
-// - name: id
-//   in: path
-//   description: ID of the recipe
-//   required: true
-//   type: string
-// produces:
-// - application/json
-// responses:
-// '200':
-// description: Successful operation
-// '400':
-//  description: Invalid input
-//  '404':
-//    description: Invalid recipe ID
-func DeleteRecipeHandler(c *gin.Context) {
-	id := c.Param("id")
-
-	index := -1
-	for i, r := range recipes {
-		if r.ID == id {
-			index = i
-		}
-	}
-	if index == -1 {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Recipe Not Found"})
-		return
-	}
-	// amazingly dumb array manipulation to keep this all in memory
-	recipes = append(recipes[:index], recipes[index+1:]...)
-
-	c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("Recipe %s has been deleted", id)})
-}
-
-// while recipes are held in memory, we'll load them from a JSON file at startup
 func init() {
-	recipes = make([]Recipe, 0)
-	file, _ := ioutil.ReadFile("recipes.json")
-	_ = json.Unmarshal([]byte(file), &recipes)
+	// connect to MongoDB
+	ctx := context.Background()
+	client, _ := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to MongoDB")
+
+	collection := client.Database(os.Getenv("MONGO_DATABASE")).Collection("recipes")
+
+	// Create route handler
+	recipesHandler = handlers.NewRecipesHandler(ctx, collection)
+
+	// uncomment and run the first time to load from .json to mongo
+	// LoadDataToDB(ctx, collection)
 }
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/recipes", ListRecipesHandler)
-	router.GET("/recipes/search", SearchRecipesHandler)
-	router.POST("/recipes", NewRecipeHandler)
-	router.PUT("recipes/:id", UpdateRecipesHandler)
-	router.DELETE("recipes/:id", DeleteRecipeHandler)
+	router.GET("/recipes", recipesHandler.ListRecipesHandler)
+	router.GET("/recipes/search", recipesHandler.SearchRecipesHandler)
+	router.POST("/recipes", recipesHandler.CreateRecipeHandler)
+	router.PUT("recipes/:id", recipesHandler.UpdateRecipesHandler)
+	router.DELETE("recipes/:id", recipesHandler.DeleteRecipeHandler)
 
 	router.Run()
+}
+
+// LoadDataToDB is a utility funciont to write sample data from json to the mongo database
+func LoadDataToDB(ctx context.Context, collection *mongo.Collection) {
+	// load recipes from file to memory
+	recipes := make([]models.Recipe, 0)
+	file, _ := ioutil.ReadFile("recipes.json")
+	_ = json.Unmarshal([]byte(file), &recipes)
+
+	// write in-memory recipes to DB
+	var listOfRecipes []interface{}
+	for _, recipe := range recipes {
+		listOfRecipes = append(listOfRecipes, recipe)
+	}
+	insertManyResult, err := collection.InsertMany(ctx, listOfRecipes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("inserted recipes: ", len(insertManyResult.InsertedIDs))
 }
