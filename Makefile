69b092ecedcee8e0748862ef4e0f1f3ff57b8753
Brandon Block <brandon.block@pm.me>
2022-07-22 15:54:29 -0500

    add react SPA

--- Makefile
+++ Makefile
@@ -1,15 +1,19 @@
+run:
+	make -j 2 run-api run-web-react
 run-api:
 	MONGO_URI="mongodb://admin:password@localhost:27017/test?authSource=admin" \
 	MONGO_DATABASE=demo \
 	REDIS_URI=localhost:6379 \
 	JWT_SECRET=eUbP9shywUygMx7u \
 	./api/api
-run-web:
+run-web-go:
 	./web-go/web
-build: build-api build-web
+run-web-react:
+	cd web-react; npm start
+build: build-api build-web-go
 build-api:
 	cd api; go build -o api
-build-web:
+build-web-go:
 	cd web-go; go build -o web
 swagger:
 	swagger generate spec -o ./api/swagger.json
--- web-react/src/App.js
+++ web-react/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Recipe from './Recipe';
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      recipes: []
+    }
+    this.getRecipes();
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (<div>
+      {this.state.recipes.map((recipe, index) => (
+        <Recipe recipe={recipe} />
+      ))}
+    </div>);
+  }
+
+  getRecipes() {
+    fetch('http://localhost:8080/recipes')
+      .then(response => response.json())
+      .then(data => this.setState({ recipes: data }))
+  }
+
 }
 
 export default App;
